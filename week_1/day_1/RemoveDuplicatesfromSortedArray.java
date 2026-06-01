64f0e6921e15aeea180aaeda973ee60cae56d4b1
Shreshtha-239 <24113cn001@glbitm.ac.in>
2026-06-01 21:53:49 +0530

    Create RemoveDuplicatesfromSortedArray.java

---
+++ week_1/day_1/RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,18 @@
+package week_1.day_1;
+
+public class RemoveDuplicatesfromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int i = 0, j = i+1, k = 1;
+        while(j <= nums.length - 1) {
+            if(nums[i] == nums[j]) {
+                j++;
+            } else {
+                i = j;
+                k++;
+                j++;
+                nums[k - 1] = nums[i];
+            }
+        }
+        return k;
+    }
+}
