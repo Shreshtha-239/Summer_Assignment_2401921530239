fe556d4757ed15a5783a5acaf5636ad45535429d
Shreshtha-239 <24113cn001@glbitm.ac.in>
2026-06-02 14:03:42 +0530

    Update MaximumAverageSubarrayI.java

--- week_one/day_two/MaximumAverageSubarrayI.java
+++ week_one/day_two/MaximumAverageSubarrayI.java
@@ -1,5 +1,16 @@
 package week_one.day_two;
 
 public class MaximumAverageSubarrayI {
-    
+    public double findMaxAverage(int[] nums, int k) {
+        double sum = 0;
+        for(int j = 0; j < k;j++) {
+            sum += nums[j];
+        }
+        double maxavg = sum/k;
+        for(int i = k;i < nums.length;i++) {
+            sum = sum - nums[i-k] + nums[i];
+            maxavg = Math.max(sum/k, maxavg);
+        }
+        return maxavg;
+    }
 }
