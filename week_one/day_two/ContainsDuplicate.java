5542e03aac527d878d5f7ab614b8fbc86246384e
Shreshtha-239 <24113cn001@glbitm.ac.in>
2026-06-02 12:41:00 +0530

    Create ContainsDuplicate.java

---
+++ week_one/day_two/ContainsDuplicate.java
@@ -0,0 +1,13 @@
+package week_one.day_two;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> arr = new HashSet<Integer>();
+        for(int i = 0;i < nums.length;i++) {
+            arr.add(nums[i]);
+        }
+        return nums.length != arr.size();
+    }
+}
