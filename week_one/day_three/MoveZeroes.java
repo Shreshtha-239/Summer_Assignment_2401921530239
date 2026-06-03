94fdde99adf2a9ba250cc6358ec65a8862aa6e25
Shreshtha-239 <24113cn001@glbitm.ac.in>
2026-06-03 13:34:45 +0530

    Create MoveZeroes.java

---
+++ week_one/day_three/MoveZeroes.java
@@ -0,0 +1,16 @@
+package week_one.day_three;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int n = 0;
+        for(int i = 0;i < nums.length;i++) {
+            if(nums[i] != 0) {
+                nums[n] = nums[i];
+                n++;
+            }
+        }
+        for(int i = n;i < nums.length;i++) {
+            nums[i] = 0;
+        }
+    }
+}
